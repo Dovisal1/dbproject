3f6647ab3880eb86eebda38bd0179f1e9bcb916e
Dovi Salomon <Dovisal1@gmail.com>
2017-10-24 11:40:03 -0400

    table schema complete. testing required.

--- schema.sql
+++ schema.sql
@@ -9,75 +9,75 @@
 -- Member
 -- Share
 
-create table content (
-	id integer not null auto increment,
+create table if not exists content (
+	cid int(10) unsigned not null auto_increment,
 	date timestamp default current_timestamp,
-	is_pub boolean, -- default? not null?
-	primary key(id)
+	is_pub boolean default false, -- default? not null?
+	primary key(cid)
 );
 
-create table person (
-	uname varchar not null,
-	password varchar,
-	fname varchar,
-	lname varchar,
+create table if not exists person (
+	uname varchar(64) not null,
+	password varchar(255) not null,
+	fname varchar(60),
+	lname varchar(60),
 	primary key(uname)
 );
 
-create table post (
-	uname varchar not null,
-	id integer not null,
-	primary key(uname, id),
+create table if not exists post (
+	uname varchar(64) not null,
+	cid int(10) unsigned not null,
+	primary key(uname, cid),
 	foreign key(uname) references person(uname),
-	foreign key(id) references content(id)
+	foreign key(cid) references content(cid)
 );
 
-create table comment (
-	uname varchar not null,
-	id integer not null,
-	timestamp date not null default current_timestamp,
-	text,
-	primary key(uname, id, timestamp),
+create table if not exists comment (
+	uname varchar(64) not null,
+	cid int(10) unsigned not null,
+	timestamp timestamp not null default current_timestamp,
+	text longtext default null,
+	primary key(uname, cid, timestamp),
 	foreign key(uname) references person(uname),
-	foreign key(id) references content(id)
+	foreign key(cid) references content(cid)
 );
 
-create table tag (
-	tagger varchar not null,
-	taggee varchar not null,
-	id integer not null,
-	timestamp date default current_timestamp,
-	status varchar, --default?
-	primary key(tagger, taggee, id),
-	foreign key(tagger) references person(uname),
-	foreign key(tagger) references person(uname),
-	foreign key(id) references content(id)
+create table if not exists tag (
+	uname_tagger varchar(64) not null,
+	uname_taggee varchar(64) not null,
+	cid int(10) unsigned not null,
+	timestamp timestamp default current_timestamp,
+	status varchar(64),
+	primary key(uname_tagger, uname_taggee, cid),
+	foreign key(uname_tagger) references person(uname),
+	foreign key(uname_tagger) references person(uname),
+	foreign key(cid) references content(cid)
 );
 
-create table friendgroup (
-	uname varchar not null,
-	name not null,
-	description varchar,
-	primary key(uname, name),
+create table if not exists friendgroup (
+	uname varchar(64) not null,
+	gname varchar(64) not null,
+	description longtext default null,
+	primary key(uname, gname),
 	foreign key(uname) references person(uname)
 );
 
-create table member (
-	owner varchar not null,
-	group_name varchar not null,
-	group_member varchar not null,
-	primary key(owner, group_name, group_member),
-	foreign key(owner, group_name) references friendgroup(uname, name),
-	foreign key(group_member) references person(uname)
+create table if not exists member (
+	uname_owner varchar(64) not null,
+	gname varchar(64) not null,
+	uname_member varchar(64) not null,
+	primary key(uname_owner, gname, uname_member),
+	foreign key(uname_owner, gname) references friendgroup(uname, gname),
+	foreign key(uname_member) references person(uname)
 );
 
-create table share (
-	owner varchar not null,
-	group_name varchar not null,
-	c_id integer not null,
-	primary key(owner, group_name, c_id),
-	foreign key(owner, group_name) references friendgroup(uname, name),
-	foreign key(c_id) references content(id)
+create table if not exists share (
+	cid int(10) unsigned not null,
+	uname_owner varchar(64) not null,
+	gname varchar(64) not null,
+	primary key(cid, uname_owner, gname),
+	foreign key(cid) references content(cid),
+	foreign key(uname_owner, gname) references friendgroup(uname, gname)
 );
 
 
