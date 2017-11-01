260a82c1bed7231c123c617bd837a891e306eb2a
Dov Salomon <dovisal1@gmail.com>
2017-11-01 17:44:57 -0400

    simplified the names of some attributes

--- sql/q.sql
+++ sql/q.sql
@@ -1,5 +1,6 @@
 
+-- Query to find the name of all content that is shared with David
 
 select name
 from content natural join share natural join member
-where uname_member = 'DD' or is_pub = true;
+where member = 'DD' or is_pub = true;
--- sql/schema.sql
+++ sql/schema.sql
@@ -15,7 +15,7 @@ create table if not exists content (
 	date timestamp default current_timestamp,
 	file_path varchar(255) default null,
 	name varchar(255) not null,
-	is_pub boolean default false, -- default? not null?
+	is_pub boolean default false,
 	primary key(cid)
 );
 
@@ -46,14 +46,14 @@ create table if not exists comment (
 );
 
 create table if not exists tag (
-	uname_tagger varchar(64) not null,
-	uname_taggee varchar(64) not null,
+	tagger varchar(64) not null,
+	taggee varchar(64) not null,
 	cid int(10) unsigned not null,
 	timestamp timestamp default current_timestamp,
 	status varchar(64),
-	primary key(uname_tagger, uname_taggee, cid),
-	foreign key(uname_tagger) references person(uname),
-	foreign key(uname_tagger) references person(uname),
+	primary key(tagger, taggee, cid),
+	foreign key(tagger) references person(uname),
+	foreign key(tagger) references person(uname),
 	foreign key(cid) references content(cid)
 );
 
@@ -66,21 +66,21 @@ create table if not exists friendgroup (
 );
 
 create table if not exists member (
-	uname_owner varchar(64) not null,
+	owner varchar(64) not null,
 	gname varchar(64) not null,
-	uname_member varchar(64) not null,
-	primary key(uname_owner, gname, uname_member),
-	foreign key(uname_owner, gname) references friendgroup(uname, gname),
-	foreign key(uname_member) references person(uname)
+	member varchar(64) not null,
+	primary key(owner, gname, member),
+	foreign key(owner, gname) references friendgroup(uname, gname),
+	foreign key(member) references person(uname)
 );
 
 create table if not exists share (
 	cid int(10) unsigned not null,
-	uname_owner varchar(64) not null,
+	owner varchar(64) not null,
 	gname varchar(64) not null,
-	primary key(cid, uname_owner, gname),
+	primary key(cid, owner, gname),
 	foreign key(cid) references content(cid),
-	foreign key(uname_owner, gname) references friendgroup(uname, gname)
+	foreign key(owner, gname) references friendgroup(uname, gname)
 );
 
 
