d4685a828228800a7a8ca29c69db3922fc499db3
Dov Salomon <dovisal1@gmail.com>
2017-11-01 22:38:25 -0400

    added sql connection using config params

--- app/views.py
+++ app/views.py
@@ -1,21 +1,27 @@
 from flask import render_template, request, redirect
 from flask import Flask
 from app import app
+import pymysql.cursors
 #from .forms import SearchForm
 #import requests
 #import json
 #import codecs
 
+conn = pymysql.connect(host=app.config['DBHOST'],
+                       user=app.config['DBUSER'],
+                       password=app.config['DBPASS'],
+                       db=app.config['DBNAME'],
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
 #Routes Index Page
 @app.route('/')
-@app.route('/index')
 @app.route('/index/')
 def index():
     return render_template("index.html",
                            title='Home')
 
 #Routes About Page
-@app.route('/about')
 @app.route('/about/')
 def about():
     return render_template("about.html",
